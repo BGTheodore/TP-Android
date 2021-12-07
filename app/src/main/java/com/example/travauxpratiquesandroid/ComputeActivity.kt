033696c7d43bd1c09831ef051677afb5ad309993
bgtheodore18@gmail.com <bgtheodore18@gmail.com>
2021-12-07 19:13:32 +0100

    Trying to make computeActivity the default one

--- app/src/main/java/com/example/travauxpratiquesandroid/ComputeActivity.kt
+++ app/src/main/java/com/example/travauxpratiquesandroid/ComputeActivity.kt
@@ -1,7 +1,12 @@
 package com.example.travauxpratiquesandroid
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 
 class ComputeActivity : AppCompatActivity(){
-
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.compute_activity)
+    }
 }
